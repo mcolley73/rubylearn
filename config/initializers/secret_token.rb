421a418f3269947209dc69ab0068759c25788a2c
Mark Colley <mcolley73@gmail.com>
2014-01-20 14:20:32 -0500

    Committing first simple changes to base RoR app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubylearn::Application.config.secret_key_base = '40d22126f19722b70ff3ceedea06c20f7df0ae9953bc58f798e82cea71ffc54aac47de868c0315bfc096c6301a3fdd44f06c6e4ddd19388b91906ebc9f6b0139'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubylearn::Application.config.session_store :cookie_store, key: '_rubylearn_session'
