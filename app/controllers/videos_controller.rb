7499b8c31804211ae466ce1d268684601db0e262
Mark Colley <mcolley73@gmail.com>
2014-01-20 15:56:36 -0500

    More playing.

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,12 @@
+class VideosController < ApplicationController
+	layout "application"
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rubylearn::Application.routes.draw do
+
   get "welcome/index"
+  root 'welcome#index'
+
+  resources :videos
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
